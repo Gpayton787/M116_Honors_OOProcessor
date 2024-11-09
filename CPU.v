e68668337850207d629baa09b49c2461d909e0d9
Gregory Payton <107384112+Gpayton787@users.noreply.github.com>
2024-11-09 15:21:56 -0800

    Update CPU.v
    Made ROM it's own module.

--- CPU.v
+++ CPU.v
@@ -3,28 +3,48 @@ module clk;
   
 endmodule
 
-
-// Code your design here
-module CPU(
-	input wire clk,
-  output wire [31:0] instruction
+module single_port_ROM 
+#(
+    parameter ADDR_WIDTH = 12,
+              DATA_WIDTH = 8
+)
+(
+  //12 bit PC input
+  input wire [ADDR_WIDTH-1: 0] addr,
+  //32 bit instruction output
+  output wire [DATA_WIDTH*4-1: 0] dout
 );
-  //1 KB instruction ROM, byte addressable, 256 instructions
-  reg [7:0] instructionROM [0:1023];
-  //PC, 1024 addresses
-  reg [9:0] PC;
+  //2^12 or 4 KB byte addressable memory
+  reg [DATA_WIDTH-1:0] mem[0:2**ADDR_WIDTH-1];
   
-  //Connect output wire
-  assign instruction = {instructionROM[PC], instructionROM[PC+1], instructionROM[PC+2], instructionROM[PC+3]}; 
-  
-  initial begin //Read trace, init values
-    PC = 0;
-    $readmemh("test.txt", instructionROM, 0, 47);
+  initial begin //Read trace, initialize ROM
+    $readmemh("test.txt", mem, 0, 47);
   end
 
+  assign dout = {mem[addr], mem[addr+1], mem[addr+2], mem[addr+3]}; 
+
+endmodule
+
+//TOP LEVEL MODULE
+module CPU(
+	input wire clk,
+  output wire [31:0] instruction //temporary
+);
+  
+  reg [11:0] PC;
+  
+  initial begin
+    PC = 0;
+  end
+  
+  //Module instantiations
+  single_port_ROM instMem(
+    .addr(PC),
+    .dout(instruction)
+  );
+  
   //Update PC on the clk
   always @(posedge clk) begin
-    
     PC <= PC + 4;
   end
 
