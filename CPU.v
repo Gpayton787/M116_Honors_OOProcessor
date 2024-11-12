b7713fe8691ae63ec9b76d832cf5b48b3fec4129
wertguk <70298499+wertguk@users.noreply.github.com>
2024-11-12 15:01:39 -0800

    fixed pcadder

--- CPU.v
+++ CPU.v
@@ -121,12 +121,13 @@ module ProgramCounter
     end
 endmodule
 
-module PCAdder(curr_pc, new_pc);
-    input wire [31:0] curr_pc;
-    output wire [31:0] new_pc;
-    always @(curr_pc)
+module PCAdder
+  (
+    input wire [31:0] curr_pc,
+    output reg [31:0] new_pc
+  );
+  	always @(curr_pc)
     begin
-        new_pc => curr_pc + 32'h00000004;
+        new_pc <= curr_pc + 32'h00000004;
     end
 endmodule
-
