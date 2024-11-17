0f5280cd1707362704165a4c54039c980c2f2ad8
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2024-11-16 17:15:09 -0800

    Updated TB

--- tb.v
+++ tb.v
@@ -1,4 +1,4 @@
-//Master Testbench
+// MASTER TB
 
 module tb;
   reg [31:0] tb_cycle_count;
@@ -6,12 +6,16 @@ module tb;
   wire [31:0] tb_instruction;
   wire [11:0] tb_pc;
   wire [6:0] tb_c_sig;
+  wire [2:0] tb_alu_sig;
+  wire [31:0] tb_imm;
   //Instantiate modules
   CPU tb_cpu(
     .clk(tb_clk),
     .fetch_instr_out(tb_instruction),
     .fetch_pc_out(tb_pc),
-    .decode_c_sig_out(tb_c_sig)
+    .decode_c_sig_out(tb_c_sig),
+    .imm_out(tb_imm),
+    .alu_sig_out(tb_alu_sig)
   );
 
   initial begin
@@ -26,7 +30,7 @@ module tb;
   
   always @(posedge tb_clk) begin
     tb_cycle_count <= tb_cycle_count + 1;
-    $display("Cycle: %0d, Fetch PC: %h, Instruction: %h | Decode C_sig: %b", tb_cycle_count, tb_pc, tb_instruction, tb_c_sig);
+    $display("Cycle: %0d, Fetch PC: %h, Instruction: %h | Decode C_sig: %b ALU_sig: %b | Imm: %d", tb_cycle_count, tb_pc, tb_instruction, tb_c_sig, tb_alu_sig, tb_imm);
   end
   
   initial begin
