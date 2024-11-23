ca810afb316dfacc8a9504a63f488c5e4c4dd0e7
Gregory Payton <107384112+Gpayton787@users.noreply.github.com>
2024-11-23 15:36:57 -0800

    Update tb.v

--- tb.v
+++ tb.v
@@ -1,26 +1,31 @@
-// MASTER TB
+// Read trace into instruction memory
 
 module tb;
   reg [31:0] tb_cycle_count;
   reg tb_clk;
-  wire [31:0] tb_instruction;
-  wire [11:0] tb_pc;
-  wire [6:0] tb_c_sig;
-  wire [2:0] tb_alu_sig;
-  wire [31:0] tb_imm;
+  reg tb_rst;
+  wire [31:0] tb_f_instr;
+  wire [31:0] tb_d_instr;
+  wire [6:0] tb_d_c_sig;
+  wire [2:0] tb_d_alu_sig;
+  wire [31:0] tb_d_imm;
+  
+  
   //Instantiate modules
   CPU tb_cpu(
     .clk(tb_clk),
-    .fetch_instr_out(tb_instruction),
-    .fetch_pc_out(tb_pc),
-    .decode_c_sig_out(tb_c_sig),
-    .imm_out(tb_imm),
-    .alu_sig_out(tb_alu_sig)
+    .rst(tb_rst),
+    .cpu_f_instr_out(tb_f_instr),
+    .cpu_d_instr_out(tb_d_instr),
+    .cpu_d_c_sig_out(tb_d_c_sig),
+    .cpu_d_imm_out(tb_d_imm),
+    .cpu_d_alu_sig_out(tb_d_alu_sig)
   );
 
   initial begin
     tb_cycle_count = 32'b0;
     tb_clk = 0;
+    tb_rst = 0;
     #5;
   end
   
@@ -30,7 +35,7 @@ module tb;
   
   always @(posedge tb_clk) begin
     tb_cycle_count <= tb_cycle_count + 1;
-    $display("Cycle: %0d, Fetch PC: %h, Instruction: %h | Decode C_sig: %b ALU_sig: %b | Imm: %d", tb_cycle_count, tb_pc, tb_instruction, tb_c_sig, tb_alu_sig, tb_imm);
+    $display("Cycle: %0d, FETCH| instr: %h DECODE| instr: %h c_sig: %b alu_sig: %b, imm: %0d", tb_cycle_count, tb_f_instr, tb_d_instr, tb_d_c_sig, tb_d_alu_sig, tb_d_imm);
   end
   
   initial begin
