08249cbab44464d8c15715f17c20fa14988f905e
Gregory Payton <107384112+Gpayton787@users.noreply.github.com>
2024-11-23 15:37:18 -0800

    Update CPU.v

--- CPU.v
+++ CPU.v
@@ -1,37 +1,95 @@
 `include "fetch.v"
 `include "fetch_buffer.v"
 `include "decode.v"
+`include "rename.v"
+`include "decode_buffer.v"
 
 //TOP LEVEL MODULE
 module CPU(
-	input wire clk,
-  output wire [31:0] fetch_instr_out,
-  output wire [11:0] fetch_pc_out, //temporary
-  output wire [6:0] decode_c_sig_out //temporary
+  input wire clk,
+  input wire rst,
+  output wire [31:0] cpu_f_instr_out,
+  output wire [31:0] cpu_d_instr_out, //temporary
+  output wire [6:0] cpu_d_c_sig_out, //temporary
+  output wire [31:0] cpu_d_imm_out, //temporary
+  output wire [2:0] cpu_d_alu_sig_out //temporary
 );
+  //Parameters
+  parameter PREG_WIDTH = 6;
+  parameter AREG_WDITH = 5;
   
-  wire [11:0] pc_decode_out;
+  //Internal wires to connect modules
+  
+  //FETCH OUTPUTS
+  wire [31:0] f_instr_out;
+  wire [11:0] f_pc_out;
+  
+  //FETCH BUFFER OUTPUTS
+  wire [31:0] fb_instr_out;
+  wire [11:0] fb_pc_out;
+  
+  //DECODE OUTPUTS
+  wire [31:0] d_instr_out;
+  wire [11:0] d_pc_out;
+  wire [6:0] d_c_sig_out;
+  wire [2:0] d_alu_sig_out;
+  wire [31:0] d_imm_out;
+  
+  //DECODE BUFFER OUTPUTS
+  wire [31:0] db_instr_out;
+  wire [11:0] db_pc_out;
+  wire [6:0] db_c_sig_out;
+  wire [2:0] db_alu_sig_out;
+  wire [31:0] db_imm_out;
+  
+  assign cpu_f_instr_out = fb_instr_out;
+  assign cpu_d_instr_out = db_instr_out;
+  assign cpu_d_c_sig_out = db_c_sig_out;
+  assign cpu_d_imm_out = db_imm_out;
+  assign cpu_d_alu_sig_out  = db_alu_sig_out;
+  
+  
   
   //Module instantiations
   fetch my_fetch(
     .clk(clk),
-    .instr_out(fetch_instr_out),
-    .pc_out(fetch_pc_out)
+    .instr_out(f_instr_out),
+    .pc_out(f_pc_out)
   );
-  /*
+  
   fetch_buffer my_fetch_buffer(
     .clk(clk),
-    .pc_in(PC),
-    .instr_in(instruction),
-    .pc_out(cpu_pc_out),
-    .instr_out(cpu_instr_out)
-  );*/
-  decode my_decode(
-    .pc_in(fetch_pc_out),
-    .instr_in(fetch_instr_out),
-    .c_sig_out(decode_c_sig_out),
-    .pc_out(pc_decode_out)
+    .pc_in(f_pc_out),
+    .instr_in(f_instr_out),
+    .pc_out(fb_pc_out),
+    .instr_out(fb_instr_out)
   );
+  
+  decode my_decode(
+    .pc_in(fb_pc_out),
+    .instr_in(fb_instr_out),
+    .c_sig_out(d_c_sig_out),
+    .alu_sig_out(d_alu_sig_out),
+    .pc_out(d_pc_out),
+    .instr_out(d_instr_out),
+    .imm(d_imm_out)
+  );
+  
+  decode_buffer my_decode_buffer(
+    .clk(clk),
+    .pc_in(d_pc_out),
+    .instr_in(d_instr_out),
+    .c_sig_in(d_c_sig_out),
+    .alu_sig_in(d_alu_sig_out),
+    .imm_in(d_imm_out),
+    .pc_out(db_pc_out),
+    .instr_out(db_instr_out),
+    .c_sig_out(db_c_sig_out),
+    .alu_sig_out(db_alu_sig_out),
+    .imm_out(db_imm_out)
+  );
+  
+  
 endmodule
 
 
