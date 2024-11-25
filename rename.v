f021bca36a34bf753a17f44fed4e99c2e950ea86
Gpayton787 <107384112+Gpayton787@users.noreply.github.com>
2024-11-24 17:12:51 -0800

    Updated ARF but still haven't tested it.

--- rename.v
+++ rename.v
@@ -140,7 +140,6 @@ endmodule
 
 module Areg_file#(
   PREG_WIDTH = 6,
-  READY_WIDTH = 1,
   DATA_WIDTH = 32,
   AREG_WIDTH = 5,
   NUM_REG = 32
@@ -151,21 +150,42 @@ module Areg_file#(
   input wire [AREG_WIDTH-1:0] rs1_tag_idx,
   input wire [AREG_WIDTH-1:0] rs2_tag_idx,
   input wire [AREG_WIDTH-1:0] rd_tag_idx,
+  //PLEASE NOTE THAT THIS REG_WRITE CORRESPONDS TO RENAMING RD NOT WRITING DATA
   input wire reg_write,
   input wire [PREG_WIDTH-1:0] rd_tag,
  
+  output reg [DATA_WIDTH-1:0] rs1_data,
+  output reg [DATA_WIDTH-1:0] rs2_data,
   output reg [PREG_WIDTH-1:0] rs1_tag,
   output reg [PREG_WIDTH-1:0] rs2_tag,
   output reg [PREG_WIDTH-1:0] rd_old_tag
 
 );
   //Declare register file memory
-  reg [PREG_WIDTH-1:0] mem[0:NUM_REG-1];
+  
+  //READY BITS
+  reg ready_mem[0:NUM_REG-1];
+  
+  //TAGS
+  reg [PREG_WIDTH-1:0] tag_mem[0:NUM_REG-1];
+  
+  //DATA
+  reg [DATA_WIDTH-1:0] data_mem[0:NUM_REG-1];
 
-  //Initialize the tags
+  //Initialize memory
   initial begin 
+    //INIT READY to all 1's
     for (integer i = 0; i < NUM_REG; i = i+1) begin
-      mem[i] = i;
+      tag_mem[i] = 1;
+    end
+    //INIT TAGS to one-to-one mapping
+    for (integer i = 0; i < NUM_REG; i = i+1) begin
+      tag_mem[i] = i;
+    end
+    
+    //INIT DATA TO 0's
+    for (integer i = 0; i < NUM_REG; i = i+1) begin
+      tag_mem[i] = 0;
     end
   end
   
@@ -173,14 +193,14 @@ module Areg_file#(
   always @(posedge clk or rst) begin
     if(rst) begin
       for (integer i = 0; i < NUM_REG; i = i+1) begin
-        mem[i] <= i;
+        tag_mem[i] <= i;
       end
     end
     
     if(reg_write) begin 
       //Ensure we can't write to x0
       if(rd_tag_idx != 0) begin
-        mem[rd_tag_idx] <= rd_tag;
+        tag_mem[rd_tag_idx] <= rd_tag;
       end
     end
 end
@@ -188,9 +208,11 @@ end
  
 //Combinational block for reads
 always @(*) begin
-  rd_old_tag = mem[rd_tag_idx];
-  rs1_tag = mem[rs1_tag_idx];
-  rs2_tag = mem[rs2_tag_idx];
+  rd_old_tag = tag_mem[rd_tag_idx];
+  rs1_tag = tag_mem[rs1_tag_idx];
+  rs2_tag = tag_mem[rs2_tag_idx];
+  rs1_data = data_mem[rs1_tag_idx];
+  rs2_data = data_mem[rs2_tag_idx];
 end
 
 endmodule
