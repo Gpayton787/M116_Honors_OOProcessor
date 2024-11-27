aa1ece3dcb310e2b2ff7dcc2efed787679af07a1
wertguk <70298499+wertguk@users.noreply.github.com>
2024-11-26 23:42:59 -0800

    Update constants.v

--- constants.v
+++ constants.v
@@ -5,3 +5,17 @@
 `define MEMWR 2
 `define MEM_TO_REG 1
 `define BYTE 0
+
+`define RS_WIDTH 132
+`define RS_ROB 5:0
+`define RS_FU 7:6
+`define RS_IMM 39:8
+`define RS_RDY2 40:40
+`define RS_DATA2 72:41
+`define RS_SRC2 78:73
+`define RS_RDY1 79:79
+`define RS_DATA1 111:80
+`define RS_SRC1 117:112
+`define RS_RD 123:118
+`define RS_OP 130:124
+`define RS_USE 131:131
