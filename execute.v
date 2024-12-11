68d6bf4e15fc7221be15357e67e60666090bca7b
wertguk <70298499+wertguk@users.noreply.github.com>
2024-12-10 23:34:20 -0800

    Update execute.v

--- execute.v
+++ execute.v
@@ -1,47 +1,106 @@
-module alu1
+`include "rs_constants.v"
+`include "alu_constants.v"
+
+module alu0
 (
-    input wire [`RS_WIDTH-1:0] rs_in;
+  input wire [`RS_WIDTH-1:0] instr_in0,
+  input wire [2:0] instr_ready,
+  input wire clk,
+  output reg [`ALU_WIDTH-1:0] result0
 );
 
 reg [6:0] opcode;
 reg [2:0] funct3;
 reg [31:0] out;
 
-always @(rs_in) begin
-    opcode = rs_in[`RS_OP];
-    funct3 = rs_in[`RS_FUNCT3];
+always @(posedge clk) begin
+    opcode <= instr_in0[`RS_OP];
+    funct3 <= instr_in0[`RS_FUNCT3];
 
-    case (opcode)
+  	case (opcode)
         7'b0110011 : begin
             if (funct3 == 3'b000) begin // add
-                out = rs_in[`RS_DATA1] + rs_in[`RS_DATA2];
+              	out <= instr_in0[`RS_DATA1] + instr_in0[`RS_DATA2];
             end
             else if (funct3 == 3'b100) begin // xor
-                out = rs_in[`RS_DATA1] ^ rs_in[`RS_DATA2];
+              	out <= instr_in0[`RS_DATA1] ^ instr_in0[`RS_DATA2];
             end
-        end; 
+        end 
         7'b0010011 : begin 
             if (funct3 == 3'b000) begin // addi
-                out = rs_in[`RS_DATA1] + rs_in[`RS_IMM];
+              	out <= instr_in0[`RS_DATA1] + instr_in0[`RS_IMM];
             end
             else if (funct3 == 3'b110) begin // ori
-                out = rs_in[`RS_DATA1] | rs_in[`RS_IMM];
+              	out <= instr_in0[`RS_DATA1] | instr_in0[`RS_IMM];
             end
             else if (funct3 == 3'b101) begin // srai
-                out = rs_in[`RS_DATA1] >>> rs_in[`RS_IMM];
+              	out <= instr_in0[`RS_DATA1] >>> instr_in0[`RS_IMM];
             end
-        end;
-        7'b0110111 : out = rs_in[`RS_IMM] << 12; //lui
+        end
+        7'b0110111 : out <= instr_in0[`RS_IMM] << 12; //lui
     endcase
+  
+  if (instr_ready[0] == 1) begin
+    result0 <= {1'b1, out, instr_in0[`RS_RD]};
+  end 
+  else begin
+    result0 <= `ALU_WIDTH'b0;
+  end
+  //$display("ALU0 Output: %d, Instruction %b", out, instr_in0[`RS_FUNCT3]);
 end
 
 endmodule
 
-module alu2
+module alu1
 (
-
+  input wire [`RS_WIDTH-1:0] instr_in1,
+  input wire [2:0] instr_ready,
+  input wire clk,
+  output reg [`ALU_WIDTH-1:0] result1
 );
+
+reg [6:0] opcode;
+reg [2:0] funct3;
+reg [31:0] out;
+
+always @(posedge clk) begin
+    opcode <= instr_in1[`RS_OP];
+    funct3 <= instr_in1[`RS_FUNCT3];
+
+  	case (opcode)
+        7'b0110011 : begin
+            if (funct3 == 3'b000) begin // add
+              	out <= instr_in1[`RS_DATA1] + instr_in1[`RS_DATA2];
+            end
+            else if (funct3 == 3'b100) begin // xor
+              	out <= instr_in1[`RS_DATA1] ^ instr_in1[`RS_DATA2];
+            end
+        end 
+        7'b0010011 : begin 
+            if (funct3 == 3'b000) begin // addi
+              	out <= instr_in1[`RS_DATA1] + instr_in1[`RS_IMM];
+            end
+            else if (funct3 == 3'b110) begin // ori
+              	out <= instr_in1[`RS_DATA1] | instr_in1[`RS_IMM];
+            end
+            else if (funct3 == 3'b101) begin // srai
+              	out <= instr_in1[`RS_DATA1] >>> instr_in1[`RS_IMM];
+            end
+        end
+        7'b0110111 : out <= instr_in1[`RS_IMM] << 12; //lui
+    endcase
+  
+  if (instr_ready[1] == 1) begin
+    result1 <= {1'b1, out, instr_in1[`RS_ROB]};
+  end 
+  else begin
+    result1 <= `ALU_WIDTH'b0;
+  end
+  //$display("ALU1 Output: %d, Instruction %b", out, instr_in1[`RS_FUNCT3]);
+end
+
 endmodule
+
 
 module memory
 (
