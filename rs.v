6a70f126f34ac41f557368d54c3e069edf4ea9f1
wertguk <70298499+wertguk@users.noreply.github.com>
2024-12-10 23:34:44 -0800

    Update rs.v

--- rs.v
+++ rs.v
@@ -1,4 +1,5 @@
 `include "rs_constants.v"
+`include "alu_constants.v"
 
 module rs#(
   PREG_WIDTH = 6,
@@ -17,6 +18,9 @@ module rs#(
   input wire ready2,
   input wire [6:0] c_sigs,
   
+  //ALU inputs
+  input wire [`ALU_WIDTH-1:0] alu0_in,
+  input wire [`ALU_WIDTH-1:0] alu1_in,
   
   //Logic inputs
   input wire [2:0] fu_table_in,
@@ -27,7 +31,7 @@ module rs#(
   output reg [`RS_WIDTH-1:0] instr_out0,
   output reg [`RS_WIDTH-1:0] instr_out1,
   output reg [`RS_WIDTH-1:0] instr_out2,
-  output reg [2:0] fu_table_update_out,
+  output reg [2:0] fu_table_out,
   output reg [2:0] instr_valid
 );
 
@@ -35,21 +39,42 @@ module rs#(
   reg [5:0] rs_pointer; //circular reservation station
   reg next_alu; //switches between first two alu
 
-  reg [6:0] opcode;
-  reg [2:0] funct3;
+  wire [6:0] opcode;
+  wire [2:0] funct3;
   reg [1:0] fu_pos;
-  reg [2:0] fu_table_temp;
+  
+  assign opcode = instr[6:0];
+  assign funct3 = instr[14:12];
 
 initial begin
   	instr_valid = 3'b000; // no instructions ready to issue
     rs_pointer = 0;
     next_alu = 2'b00;
-    fu_table_temp = 3'b111; // all fu start out ready
+  	fu_table_out = 3'b111;
 end
+  
+always @(negedge clk) //writeback from alu
+  begin
+    for (integer i = 0; i < 64; i = i+1) begin
+      if (queue[i][`RS_SRC1] == alu0_in[`ALU_REG])
+        begin
+          queue[i][`RS_DATA1] <= alu0_in[`ALU_RESULT];
+          queue[i][`RS_RDY1] <= 1;
+        end
+      if (queue[i][`RS_SRC2] == alu0_in[`ALU_REG])
+        begin
+          queue[i][`RS_DATA2] <= alu1_in[`ALU_RESULT];
+          queue[i][`RS_RDY2] <= 1;
+       	end
+    end
+  end
 
 always @(posedge clk) //issue
 begin
-  instr_valid = 3'b000;
+  fu_table_out <= fu_table_in;
+  instr_valid <= 3'b000;
+ 	
+  //Issue logic
   for (integer i = 0; i < 64; i = i+1) begin
     if (
         queue[i][`RS_USE] == 1 //
@@ -59,52 +84,44 @@ begin
     ) // check fu ready and src reg ready
      	begin
           //Update FU to used
-          fu_table_temp[queue[i][`RS_FU]] = 0;
-          if (instr_valid[0] == 0) begin
-            instr_out0 = queue[i];
-            instr_valid[0] = 1;
+          fu_table_out[queue[i][`RS_FU]] <= 0;
+          if (queue[i][`RS_FU] == 2'b00) begin
+            instr_out0 <= queue[i];
+            instr_valid[0] <= 1;
           end
-          else if (instr_valid[1] == 0) begin
-            instr_out1 = queue[i];
-            instr_valid[1] = 1;
+          else if (queue[i][`RS_FU] == 2'b01) begin
+            instr_out1 <= queue[i];
+            instr_valid[1] <= 1;
           end
-          else if (instr_valid[2] == 0) begin
-            instr_out2 = queue[i];
-            instr_valid[2] = 1;
+          else if (queue[i][`RS_FU] == 2'b10) begin
+            instr_out2 <= queue[i];
+            instr_valid[2] <= 1;
           end
-          queue[i][`RS_USE] = 0;
+          queue[i][`RS_USE] <= 0;
         end
-    end
-    fu_table_update_out = fu_table_temp;
+    end 
 end
 
   always @(posedge clk) //dispatch
 begin
-    opcode = instr[6:0];
-  funct3 = instr[14:12];
 
-    case (opcode)
+  	case (opcode)
         7'b0000011 : fu_pos = 2'b10; //lb, lw
         7'b0100011 : fu_pos = 2'b10; //sb, sw
         default : begin
-            fu_pos = next_alu;
-            next_alu = !next_alu;
+            fu_pos <= next_alu;
+            next_alu <= !next_alu;
         end
     endcase
   
   	//If opcode is not zero, reserve an entry
-    if(opcode!=0) begin
-      queue[rs_pointer] = {1'b1, funct3, c_sigs, opcode, rd, src1, data1, ready1, src2, data2, ready2, imm, fu_pos, rob_num};
+  	if(opcode!=0) begin
+      queue[rs_pointer] <= {1'b1, funct3, c_sigs, opcode, rd, src1, data1, ready1, src2, data2, ready2, imm, fu_pos, rob_num};
       
-      $display("Reserving entry | f3: %b, c_sigs: %b, opcode: %b, rd: %0d, src1: %0d, data1: %h, ready1: %b, src2: %0d, data2: %h, ready2: %b, imm: %0d, fu_pos: %b, rob_num, %h", funct3, c_sigs, opcode, rd, src1, data1, ready1, src2, data2, ready2, imm, fu_pos, rob_num);
 
-        rs_pointer = rs_pointer + 1;
+        rs_pointer <= rs_pointer + 1;
     end
-end
-  
-always @(posedge clk)
-begin
-    fu_table_temp = fu_table_in;
+  //$display("Reserving entry | f3: %b, c_sigs: %b, opcode: %b, rd: %0d, src1: %0d, data1: %h, ready1: %b, src2: %0d, data2: %h, ready2: %b, imm: %0d, fu_pos: %b, rob_num, %h", funct3, c_sigs, opcode, rd, src1, data1, ready1, src2, data2, ready2, imm, fu_pos, rob_num);
 end
   
 endmodule
