11b2199f49f4f63004d900294e7810c270dad944
wertguk <70298499+wertguk@users.noreply.github.com>
2024-12-11 23:39:42 -0800

    Update alu_constants.v

--- alu_constants.v
+++ alu_constants.v
@@ -1,5 +1,6 @@
 //Constants to index into alu output
-`define ALU_WIDTH 39
+`define ALU_WIDTH 40
 `define ALU_REG 5:0
 `define ALU_RESULT 37:6
-`define ALU_READY 38:38
+`define ALU_WRITE 38:38
+`define ALU_READY 39:39
