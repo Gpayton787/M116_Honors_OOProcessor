d21fe43c0dcf7df33ac8c753d012296e66018ad9
wertguk <70298499+wertguk@users.noreply.github.com>
2024-12-11 23:42:14 -0800

    Updates from alu

--- fu_table.v
+++ fu_table.v
@@ -2,28 +2,25 @@ module fu_table
 (
   input wire clk,
   input wire rst,
+  input wire alu0,
+  input wire alu1,
   input wire [2:0] update_in,
   output reg [2:0] table_out
 );
-
-  reg [2:0] fu_table;
-
+  
   initial begin
       //All FUs start ready
-      fu_table = 3'b111;
+      table_out = 3'b111;
   end
 
-  always @(posedge clk) begin 
+  always @(negedge clk) begin 
     if(rst) begin	
-      fu_table <= 3'b111;
+       table_out <= 3'b111;
     end 
     else begin
-    	fu_table <= update_in;
+    	table_out <= update_in;
+      	table_out[0] <= table_out[0] | alu0;
+      	table_out[1] <= table_out[1] | alu1;
 	end
   end
-  
-  always @(*) begin
-    table_out = fu_table; // Continuous assignment for output
-  end
-
 endmodule
